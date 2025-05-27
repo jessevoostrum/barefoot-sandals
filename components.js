b06005e28efbf6dfc8c4e3b1645aa7922022615e
Jesse van Oostrum <jessevoostrum@gmail.com>
2025-05-27 12:59:42 +0200

    favicon

--- components.js
+++ components.js
@@ -20,6 +20,7 @@ const components = {
     head: `
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/png" href="images/logo-foot-color-brown.png">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Quicksand:wght@400;500&display=swap" rel="stylesheet">
