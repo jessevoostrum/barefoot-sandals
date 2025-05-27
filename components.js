44daf2f106a2d62505318c66cadb81fee5600ee0
Jesse van Oostrum <jessevoostrum@gmail.com>
2025-05-27 13:21:23 +0200

    edits

--- components.js
+++ components.js
@@ -9,7 +9,7 @@ const components = {
     footer: `
         <div class="footer">
             <nav>
-                <div class="footer-link"><a href="policies.html#delivery">Delivery</a></div>
+                <div class="footer-link"><a href="policies.html">Delivery</a></div>
                 <div class="footer-link"><a href="policies.html#returns">Return Policy</a></div>
                 <div class="footer-link"><a href="policies.html#refunds">Refund and Dispute Policy</a></div>
                 <div class="footer-link"><a href="https://mailhide.io/e/GfrOzBL1">Contact</a></div>
@@ -20,7 +20,6 @@ const components = {
     head: `
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="icon" type="image/png" href="images/logo-foot-color-brown.png">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Quicksand:wght@400;500&display=swap" rel="stylesheet">
